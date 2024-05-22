ba6e8e77f96b49ffd70916e601a8fca1ff7c6cf9
Edson Carreon <edsonjaybcarreon@gmail.com>
2024-05-23 01:17:10 +0800

    Add background dots to body className

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} bg-dots bg-opacity-20`}>
         <Providers>{children}</Providers>
         <Toaster />
       </body>
