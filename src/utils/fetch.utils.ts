d89a87dc37e976bca77453fb3235f6cb1f2789fd
Edson Carreon <edsonjaybcarreon@gmail.com>
2024-07-16 21:58:02 +0800

    chore: Refactor fetchRequest function to use options object

--- src/utils/fetch.utils.ts
+++ src/utils/fetch.utils.ts
@@ -2,18 +2,20 @@
  * Performs an HTTP request using the Fetch API.
  *
  * @param url The URL for the request.
- * @param method The HTTP method (GET, POST, PUT, DELETE, etc.)
- * @param data The data to be sent as body (for POST, PUT) or query parameters (for GET).
- * @param headers Additional HTTP headers to be sent with the request.
+ * @param options Configuration options for the request.
  * @returns A promise that resolves with the response.
  */
 async function fetchRequest(
   url: string,
-  method: "GET" | "POST" | "PUT" | "DELETE" | "PATCH",
-  data?: Record<string, any>,
-  headers?: Record<string, string>
+  options: {
+    method: "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
+    data?: Record<string, any>;
+    headers?: Record<string, string>;
+  }
 ): Promise<Response> {
-  const baseURL = "http://localhost:3001";
+  const { method, data, headers } = options;
+  const baseURL =
+    process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3001";
 
   url = `${baseURL}${url}`;
   const config: RequestInit = {
@@ -27,9 +29,6 @@ async function fetchRequest(
   const token = localStorage.getItem("token");
 
   if (token) {
-    if (!config.headers) {
-      config.headers = {};
-    }
     (config.headers as Record<string, string>)[
       "Authorization"
     ] = `Bearer ${token}`;
