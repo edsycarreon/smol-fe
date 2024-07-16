5135ed58f1d976c99fc5ab9950ead13dc6da9487
Edson Carreon <edsonjaybcarreon@gmail.com>
2024-07-17 01:07:37 +0800

    refactor: Update URLTable component to fetch and display user URLs using react-query

--- src/app/Home/components/data-table/page.tsx
+++ src/app/Home/components/data-table/page.tsx
@@ -1,35 +1,49 @@
+import { useQuery } from "@tanstack/react-query";
 import { useMemo, useState } from "react";
 
+import { useToast } from "../../../../components/ui/use-toast";
+import { getAllUserUrls } from "../../../../services/url/url.service";
 import { Link } from "../../../../types";
 
 import { getColumns } from "./columns";
 import { DataTable } from "./data-table";
 
 export default function URLTable() {
-  const [data, setData] = useState<Link[]>([
-    {
-      id: 1,
-      originalUrl: "https://www.youtube.com/watch?v=nbC-ErVHhzE&t=855s",
-      createdAt: "2024-07-06T13:49:16.553Z",
-      shortUrl: "http://localhost:3000/abc123",
-      expiresIn: "2024-07-08T13:49:16.553Z",
-      views: 100,
-    },
-    {
-      id: 2,
-      originalUrl: "https://youtu.be/f_iQRO5BdCM?si=bE9eJOtIr3i3VxRz",
-      createdAt: "2024-07-06T13:49:16.553Z",
-      shortUrl: "http://localhost:3000/iuyandi",
-      expiresIn: "2024-07-09T13:49:16.553Z",
-      views: 120,
-    },
-  ]);
+  const [data, setData] = useState<Link[]>();
+  const { toast } = useToast();
+
+  async function fetchLinks() {
+    const response = await getAllUserUrls();
+    return response.json();
+  }
+
+  const {
+    isPending,
+    isError,
+    data: queryData,
+    error,
+  } = useQuery({
+    queryKey: ["links"],
+    queryFn: fetchLinks,
+  });
 
   const columns = useMemo(() => getColumns(), []);
 
+  if (isPending) {
+    return null;
+  }
+
+  if (isError) {
+    toast({
+      variant: "destructive",
+      title: "Uh oh! Something went wrong. Please try again.",
+      duration: 2000,
+    });
+  }
+
   return (
     <div className="container mx-auto py-10">
-      <DataTable columns={columns} data={data} />
+      <DataTable columns={columns} data={queryData.data} />
     </div>
   );
 }
